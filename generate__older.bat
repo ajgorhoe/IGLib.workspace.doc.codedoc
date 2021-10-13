3e86e1fb4af1e2e5434b78323cd59795b404184c
igor <gresovnik@gmail.com>
2021-10-13 11:08:27 +0200

    Added test documentation to code_documentation.html, updated test configuration for more consistency.

---
+++ generate__older.bat
@@ -0,0 +1,30 @@
+
+set docname=%1
+
+rem This script generates documentation for %docname% in Doxygen. 
+rem The script must be run in the directory that contains
+rem the script.
+
+time /T
+
+
+echo 
+echo 
+echo NEW GENERATION SCRIPT.
+echo 
+echo 
+
+
+echo Generating code documentation for %docname% by Doxygen...
+echo
+
+md generated
+md generated_with_sources
+
+SET PATH=C:\Program Files (x86)\Graphviz2.38\bin;%PATH%
+
+
+CALL .\copyinfo.bat
+CALL doxygen %docname%.dox
+
+time /T
