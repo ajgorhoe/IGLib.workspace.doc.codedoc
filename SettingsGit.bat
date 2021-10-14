b2a1797ff86aeee34845b677df51cd8bb93e4c36
igor <gresovnik@gmail.com>
2021-10-14 09:55:48 +0200

    Updated SettingsGit.bat.

--- SettingsGit.bat
+++ SettingsGit.bat
@@ -1,15 +1,39 @@
 @echo off 
-rem Prints out some Git related info.
-
-setlocal
+rem Stores some Git related info in env. variables.
 
 echo.
 echo ======================================== %~n0%~x0:
-echo Git environment: 
 echo.
 
-echo.
-if 1 NEQ 10(
+rem Storing git info for the checked out repository in variables...
+
+for /f "tokens=*" %%g in ('git rev-parse --abbrev-ref HEAD') DO (set CurrentBranchName=%%g)
+
+for /f "tokens=*" %%g in ('git rev-parse HEAD') DO (set CurrentCommitHash=%%g)
+set ShortenedCommitHash=%CurrentCommitHash:~0,8%
+
+rem replace unwanted characters in branch name:
+set NormalizedBranchName=%NormalizedBranchName:"=_%
+set NormalizedBranchName=%NormalizedBranchName:(=_%
+set NormalizedBranchName=%NormalizedBranchName:)=_%
+set NormalizedBranchName=%NormalizedBranchName:/=.%
+
+if 1 NEQ 0 (
+    echo.
+	echo Git info:
+	echo CurrentBranchName: 
+	echo   %CurrentBranchName%
+	echo NormalizedBranchName: 
+	echo   %NormalizedBranchName%
+	echo CurrentCommitHash:
+	echo   %CurrentCommitHash%
+	echo Shortened commit hash:
+	echo   %ShortenedCommitHash%
+	echo.
+)
+
+if 0 NEQ 0 (
+    echo.
 	echo Current branch name: 
 	git rev-parse --abbrev-ref HEAD
 	echo Current commit hash:
