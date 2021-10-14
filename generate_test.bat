8915cf8a8a96c3fe5da623ecfd56846c9e1bd4c3
igor <gresovnik@gmail.com>
2021-10-14 11:26:19 +0200

    Supplementing generate_test.bat.

--- generate_test.bat
+++ generate_test.bat
@@ -4,19 +4,49 @@ rem Generates code documentation for the specified configuraton.
 
 rem Create local context to avoid side effects:
 setlocal
+rem Define some script-related varibles:
+set InitialDir=%CD%
+set ThisScriptDir=%~dp0
+set ExePath=%ThisScriptDir%%0
+
+rem Define configuration parameters for this specific script:
+set ConfigurationID=test
+set IsSourcesIncluded=0
+set ConfigurationSubdir=.\
+set OldGenerationMode=1
+
+echo.
+echo ======================================== %~n0%~x0:
+echo Generating code doc., conf. %ConfigurationID% ...
+echo.
 
 
-rd /s /q generated\test\html\
-
-if 1 NEQ 0 (
-  measuretime generate.bat test
+if %OldGenerationMode% NEQ 0 (
+  rem Old way of creating code dovumentation:
+  echo.
+  echo Generating code documentation rhe old way:
+  echo.
+  measuretime generate.bat %ConfigurationID%
   goto finalize
-) else (
+) 
 
-  GenerateCodeDoc.bat test
-  
+rem Execute the script that generates the configured code 
+rem documentation (the new way):
+echo.
+echo running the generation script:
+echo.
+echo call "%ThisScriptDir%generate.bat" %ConfigurationID% %IsSourcesIncluded% %ConfigurationSubdir% %*
+call "%ThisScriptDir%generate.bat" %ConfigurationID% %IsSourcesIncluded% %ConfigurationSubdir% %*
+
+IF %ERRORLEVEL% NEQ 0 (
+  echo.
+  echo An error occurred in %0:
+  echo   ERRORLEVEL = %ERRORLEVEL%
+  echo   Error message: %ErrorMessage%
+  echo.
 )
 
 :finalize
 
+rem end local context:
 endlocal
