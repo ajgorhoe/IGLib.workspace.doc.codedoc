3c03dc49de9ccbdecb6ab529627e0807006f0420
igor <gresovnik@gmail.com>
2021-10-14 10:06:45 +0200

    Using measuretime,bat in generate_test.bat.

--- generate_test.bat
+++ generate_test.bat
@@ -1,6 +1,18 @@
+@echo off
+
+rem Generates code documentation for the specified configuraton.
+
+rem Create local context to avoid side effects:
+setlocal
 
 
 rd /s /q generated\test\html\
 
-generate.bat test
+if 1 NEQ 0 (
+  measuretime generate.bat test
+  goto finalize
+)
 
+:finalize
+
+endlocal
