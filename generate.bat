47d01c7824d554516192e4b7b2f123b40cc5a8af
igor <gresovnik@gmail.com>
2021-10-16 13:00:17 +0200

    Some consolidation for legacy specific scripts.

--- generate.bat
+++ generate.bat
@@ -1,4 +1,22 @@
 
+REM The legacy general script for generation of code documentation, adapted
+REM to new way of generation.
+REM This enables use of the older specific generation scripts (like 
+REM generate_simulationtools.bat) that have not switched to the newed general
+REM generation script.
+REM Script arguments:
+REM   Arg. 1: ID of documentation configuration (expected but one can define REM     environment variable ConfigurationID instead).
+REM   Arg. 2: whether source code is included in the generation (optional,
+REM     default is 1); must coincide witth actual configuration parameter
+REM     SOURCE_BROWSER in the .dox file.
+
+REM The current script just redirects the call to the GenerateCodeDoc.bat
+REM script, after adaptation of parameters.
+
+REM The simpler original legacy script is saved in generate__older.bat .
+
+setlocal 
+
 set docname=%1
 
 rem This script generates documentation for %docname% in Doxygen. 
@@ -10,7 +28,7 @@ time /T
 
 echo 
 echo 
-echo NEW GENERATION SCRIPT.
+echo OLD GENERATION SCRIPT.
 echo 
 echo 
 
@@ -25,6 +43,20 @@ SET PATH=C:\Program Files (x86)\Graphviz2.38\bin;%PATH%
 
 
 CALL .\copyinfo.bat
-CALL doxygen %docname%.dox
+
+REM Older way:
+REM CALL doxygen %docname%.dox
+
+REM New way - delegate generation to the new script:
+rem take into account script arguments:
+set NumArgs=0
+for %%d in (%*) do set /A NumArgs+=1
+if %NumArgs% GEQ 1 (set ConfigurationID=%1)
+if %NumArgs% GEQ 2 (set IsSourcesIncluded=%2) 
+rem Take default value if parameter is not defined at this point:
+if not defined IsSourcesIncluded (set IsSourcesIncluded=0)
+rem Delegate generation to the new script:
+call "%~dp0\GenerateCodeDoc.bat" %*
+endlocal
 
 time /T
