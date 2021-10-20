50adde47c68f3e1619e712bca3a0cd96c71b13fd
Igor Grešovnik <gresovnik@gmail.com>
2021-10-20 10:56:05 +0200

    Added codedoc. removal script.

---
+++ RemoveGeneratedDocumentation.bat
@@ -0,0 +1,29 @@
+@echo off
+rem Removes the generated code documentaton.
+
+rem Prevent side effects:
+setlocal
+
+rem Reset error level:
+ver > nul
+echo.
+echo ======================================== %~n0%~x0:
+
+rem Set some script related variables.
+set ScriptDir=%~dp0
+SET InitialDir=%CD%
+
+:Finalize
+IF %ERRORLEVEL% NEQ 0 (
+  echo.
+  echo An error occurred in %0:
+  echo   ERRORLEVEL = %ERRORLEVEL%
+  echo   Error message: %ErrorMessage%
+  echo.
+)
+echo.
+echo ======== End: %~n0%~x0
+echo.
+rem End local contect for env. variables:
+endlocal
+
