ff148e84641a3760b73294784d6694d1e73b458d
Igor Grešovnik <gresovnik@gmail.com>
2025-07-04 20:54:11 +0200

    GenerateCodeDoc.ps1: defiing relevant paths, printing out relevant parameters

--- GenerateCodeDoc.ps1
+++ GenerateCodeDoc.ps1
@@ -35,10 +35,19 @@ param (
     [switch]$IsSourcesIncluded,
     [switch]$LaunchDoc
 )
+function Get-CanonicalAbsolutePath {
+    param([string]$Path)
+    if ([System.IO.Path]::IsPathRooted($Path)) {
+        return [System.IO.Path]::GetFullPath($Path)
+    } else {
+        return [System.IO.Path]::GetFullPath((Join-Path -Path (Get-Location) -ChildPath $Path))
+    }
+}
 
 Write-Host ""
 Write-Host "============================================================"
 Write-Host "GenerateCodeDoc.ps1:"
+Write-Host ""
 
 # Prefix used for setting/retrieving global variables
 $ParameterGlobalVariablePrefix = "CodeDoc_"
@@ -46,6 +55,13 @@ $ParameterGlobalVariablePrefix = "CodeDoc_"
 $scriptPath = $MyInvocation.MyCommand.Path
 $scriptDir = Split-Path $scriptPath -Parent
 
+# Update the binaries repository, ../codedoc_resources:
+$updateBinariesScript = (Join-Path $scriptDir "UpdateRepo_codedoc_resources.ps1")
+$binariesDir = (Join-Path $scriptDir "../codedoc_resources/")
+$binariesDirGit = (Join-Path $binariesDir ".git/")
+# Canonize the above paths:
+$binariesDir = (Get-CanonicalAbsolutePath $binariesDir)
+$binariesDirGit =  (Get-CanonicalAbsolutePath $binariesDirGit)
 
 Write-Host "-----------------------------------------"
 Write-Host "Script parameters:"
@@ -53,10 +69,25 @@ Write-Host "  ConfigurationId:   $ConfigurationId"
 Write-Host "  IsSourcesIncluded: $IsSourcesIncluded"
 Write-Host "  LaunchDoc:         $LaunchDoc"
 Write-Host "Global variables:"
+
+Write-Host "Derived parameters:"
+Write-Host "  scriptPath:   $scriptPath"
+Write-Host "  scriptDir:    $scriptDir"
+Write-Host "  updateBinariesScript: $updateBinariesScript"
+Write-Host "  binariesDir         : $binariesDir"
+Write-Host "  binariesDirGit      : $binariesDirGit"
 Write-Host "-----------------------------------------"
+Write-Host ""
+
+Write-Host "Updating binaries repo (`"UpdateRepo_codedoc_resources.ps1`")..."
 
 
+Write-Host "Creating code documentation for configuration `"$ConfigurationId`"..."
+# & $updateBinariesScript
 
+
+Write-Host ""
+Write-Host "Code documentation completed for `"$ConfigurationId`"."
 Write-Host "-------------------------------------------------------------"
 Write-Host ""
 
