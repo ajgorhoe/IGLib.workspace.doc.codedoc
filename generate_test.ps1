e676c17e1e34b7faa9d2f7d803b041e4af676c14
Igor Grešovnik <gresovnik@gmail.com>
2025-07-06 04:07:31 +0200

    Updated generate_test.ps1, which sets standard for PowerShell documentation gen3erating scripts

--- generate_test.ps1
+++ generate_test.ps1
@@ -1,11 +1,14 @@
 <#
 .SYNOPSIS
-    Script for generating a TEST documentation (configuration ID "test"; file test.dox). 
-
-.NOTES
-    Copyright © Igor Grešovnik.
-    Part of IGLib: https://github.com/ajgorhoe/IGLib.workspace.doc.codedoc
-	License: https://github.com/ajgorhoe/IGLib.workspace.doc.codedoc/blob/master/LICENSE.md
+    Script for generating a TEST documentation (configuration ID "test"; 
+    file test.dox).
+    Except ConfigurationId, which is fixed, this script has the same 
+    parameters as GenerateCodeDoc.ps1, which it calls. Overriding their
+    default values changes the behavior of code generation, however, this
+    is not needed in most cases. Exception is the LaunchDoc parameter, which 
+    should be set to false wheen this script is called in CI/CD pipeline.
+    Parameter $IsSourcesIncluded is also fixed, because it is determined by
+    the configuration used.
 
 .DESCRIPTION
     This file generates documentation for a small experimental code project 
@@ -14,25 +17,17 @@
     experiment with different settings and observe the effects. To browse
     generated code documentation, open generated/test/html/index.html.
 
-.PARAMETER Directory
-    The local directory.
-
-
-.EXAMPLE
-    .\UpdateOrCloneRepository.ps1 -Directory "C:\Repos\Example" -Address "https://github.com/foo/bar.git" -Execute
-
-    Clones or updates the repo in C:\Repos\Example, using the default remote name 'origin',
-    and prints status messages to the console.
-
 #>
 
+$ConfigurationId = "test"
+$IsSourcesIncluded = $true
 param (
-    [string]$ConfigurationId = "test",
-    [bool]$IsSourcesIncluded = $false,
     [bool]$ForceUpdates = $false,
     [bool]$LaunchDoc = $true
 )
 
+# Get the script path to determine the path of the documentation generating
+# script:
 $scriptPath = $MyInvocation.MyCommand.Path
 $scriptDir = Split-Path $scriptPath -Parent
 $generationScript = (Join-Path $scriptDir "GenerateCodeDoc.ps1")
@@ -41,5 +36,3 @@ $generationScript = (Join-Path $scriptDir "GenerateCodeDoc.ps1")
     -IsSourcesIncluded $IsSourcesIncluded `
     -LaunchDoc $LaunchDoc  `
     $ForceUpdates $ForceUpdates  
-    
-
