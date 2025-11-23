79312ee9929ad147db52e443d759436929ca81ee
Igor Grešovnik <gresovnik@gmail.com>
2025-11-23 19:27:24 +0100

    Adapted the script for generating all flavors of documentation

--- GenerateCodeDoc_AllFlavors.ps1
+++ GenerateCodeDoc_AllFlavors.ps1
@@ -7,17 +7,17 @@ $scriptPath = $MyInvocation.MyCommand.Path
 $scriptDir = Split-Path $scriptPath -Parent
 $scriptFilename = [System.IO.Path]::GetFileName($scriptPath)
 
-Write-Host "`nGenerating BASIC module documentation:`n"
-& $(Join-Path $scriptDir "GenerateModuleDoc.ps1")
+Write-Host "`nGenerating BASIC IGLib documentation:`n"
+& $(Join-Path $scriptDir "GenerateDocIGLib.ps1")
 
-Write-Host "`nGenerating EXTENDED module documentation:`n"
-& $(Join-Path $scriptDir "GenerateModuleDocAll.ps1")
+Write-Host "`nGenerating EXTENDED IGLib documentation:`n"
+& $(Join-Path $scriptDir "GenerateDocIGLibAll.ps1")
 
-Write-Host "`nGenerating BASIC module documentation WITH SOURCES:`n"
-& $(Join-Path $scriptDir "GenerateModuleDocWithSources.ps1")
+Write-Host "`nGenerating BASIC IGLib documentation WITH SOURCES:`n"
+& $(Join-Path $scriptDir "GenerateDocIGLibWithSources.ps1")
 
-Write-Host "`nGenerating EXTENDED module documentation WITH SOURCES:`n"
-& $(Join-Path $scriptDir "GenerateModuleDocAllWithSources.ps1")
+Write-Host "`nGenerating EXTENDED IGLib documentation WITH SOURCES:`n"
+& $(Join-Path $scriptDir "GenerateDocIGLibAllWithSources.ps1")
 
 
 Write-Host "  ... generation of ALL flavors of code documentation completed.`n`n"
